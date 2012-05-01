c6e59afa99e7fa62a735c621bb44740f5c626df8
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/JasmineBddContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/JasmineBddContrib/build.pl
+++ lib/Foswiki/Contrib/JasmineBddContrib/build.pl
@@ -2,22 +2,22 @@
 use strict;
 
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
 package JSCalendarBuild;
 
-@JSCalendarBuild::ISA = ( "Foswiki::Contrib::Build" );
+@JSCalendarBuild::ISA = ("Foswiki::Contrib::Build");
 
 sub new {
-  my $class = shift;
-  return bless( $class->SUPER::new( "JasmineBddContrib" ), $class );
+    my $class = shift;
+    return bless( $class->SUPER::new("JasmineBddContrib"), $class );
 }
 
 my $build = new JSCalendarBuild();
 
-$build->build($build->{target});
+$build->build( $build->{target} );
